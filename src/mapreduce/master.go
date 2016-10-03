50f4bca5f61d250eed6319436d5164759f21dc57
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-10-03 18:25:16 -0400

    Added simple failure handling

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -2,12 +2,11 @@ package mapreduce
 
 import "container/list"
 import "fmt"
-//import "sync"
+import "sync"
 
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
-	available bool
 }
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
@@ -30,15 +29,20 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
-	DoJobRoutine := func(w *WorkerInfo, jobArgs *DoJobArgs) {
+	var wg sync.WaitGroup
+	DoJobRoutine := func(w *WorkerInfo, jobArgs *DoJobArgs, cJobs int) {
 		var jobReply DoJobReply
 		ok := call(w.address, "Worker.DoJob", jobArgs, &jobReply)
 		if ok == false {
-			fmt.Printf("DoJob %d error\n", jobArgs.JobNumber)
+			mr.failedJobChan <-jobArgs.JobNumber
 		}   else {
-			fmt.Printf("DoJob %d done\n", jobArgs.JobNumber)
+			mr.nextAvailWorker <- w.address
+			//mutex.Lock()
+			//mutex.Unlock()
+			mr.nCompleted ++
 		}
-		mr.nextAvailWorker <- w.address
+		fmt.Println(mr.nCompleted)
+		wg.Done()
 	}
 
 	fmt.Println("Start running master")
@@ -47,30 +51,49 @@ func (mr *MapReduce) RunMaster() *list.List {
 	go func() {
 		for {
 			worker := <-mr.registerChannel
-			mr.Workers[worker] = &WorkerInfo{address: worker, available: true}
-			mr.nAvailable++
+			mr.Workers[worker] = &WorkerInfo{address: worker}
 			fmt.Printf("Worker %v is registered\n", worker)
 			mr.nextAvailWorker <- worker
-			//mr.nextAvailWorker <- worker
 		}
 	}()
 
-
+	var nextJob int
 	// start the map phase
-	for job := 0; job < mr.nMap; job++ {
+	for job := 0; job < mr.nMap+1; job++ {
+		select {
+			case nextJob = <-mr.unfinishedJobs:
+			case availWorker := <-nextAvailWorker:
+			default:
+				nextJob = job
+		}
+		if nextJob >= mr.nMap { break }
 		availWorker := <-mr.nextAvailWorker
 			// do map job
 		w := mr.Workers[availWorker]
-		mapArgs := &DoJobArgs{mr.file, Map, job, mr.nReduce}
-		go DoJobRoutine(w, mapArgs)
+		mapArgs := &DoJobArgs{mr.file, Map, nextJob, mr.nReduce}
+		wg.Add(1)
+		go DoJobRoutine(w, mapArgs, nMap)
 	}
+	wg.Wait()
+
+	mr.nCompleted = 0
 	// start the reduce phase
-	for job := 0; job < mr.nReduce; job++ {
+	for job := 0; job < mr.nReduce+1; job++ {
+		select {
+			case nextJob = <-mr.failedJobChan:
+				job --
+			default:
+				nextJob = job
+		}
+		if nextJob >= mr.nReduce { break }
         availWorker := <-mr.nextAvailWorker
             // do map job
         w := mr.Workers[availWorker]
         reduceArgs := &DoJobArgs{mr.file, Reduce, job, mr.nMap}
-		go DoJobRoutine(w, reduceArgs)
+		//wg.Add(1)
+		go DoJobRoutine(w, reduceArgs, nReduce)
 	}
+	//wg.Wait()
+
 	return mr.KillWorkers()
 }
